10f0b7cbdef4bed9154d3b8ed295f7cb7d561171
Pietropaolo Frisoni <68463840+PietropaoloFrisoni@users.noreply.github.com>
2021-10-03 00:05:06 -0400

    Update default configurations to compute

--- configs_to_compute.jl
+++ configs_to_compute.jl
@@ -1,5 +1,5 @@
 # folder where draws, ampls and operators will be stored
-data_folder_path = "/home/frisus95/Scrivania"
+data_folder_path = pwd()
 
 # verbosity of execution flux, 0 is minimum, 2 is maximum
 verbosity_flux = 2
@@ -8,14 +8,17 @@ verbosity_flux = 2
 verbosity_random_walk = 1
 
 # combine every computed chain, assemble all data in a csv file (combining all computed configurations) and save it in main folder
-assemble_and_save_final_data = false
+assemble_and_save_final_data = true
 
 # print final combined data on terminal after execution  
-print_final_data_on_terminal = false
+print_final_data_on_terminal = true
 
 # usage example
-Configurations = [[0.5, "BF", 10^6, 10^3, 0.40, [false, false], [true, true], [true, true, 1, 2]],
-                  [1.0, "BF", 10^6, 10^3, 0.39, [false, false], [true, true], [true, true, 1, 2]],
-                  [1.5, "BF", 10^6, 10^3, 0.37, [false, false], [true, true], [true, true, 1, 2]],
+Configurations = [[0.5, "BF", 10^6, 10^3, 0.40, [true, false], [true, true], [true, true, 1, 2]],
+                  [1.0, "BF", 10^6, 10^3, 0.39, [true, false], [true, true], [true, true, 1, 2]],
+                  [1.5, "BF", 10^6, 10^3, 0.37, [true, false], [true, true], [true, true, 1, 2]],
+                  [0.5, "EPRL", 10^6, 10^3, 0.40, [true, false], [true, true], [true, true, 1, 2]],
+                  [1.0, "EPRL", 10^6, 10^3, 0.39, [true, false], [true, true], [true, true, 1, 2]],
+                  [1.5, "EPRL", 10^6, 10^3, 0.37, [true, false], [true, true], [true, true, 1, 2]],  
                  ]  
 
